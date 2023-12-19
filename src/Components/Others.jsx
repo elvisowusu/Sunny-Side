c1fa1e548f50f7db39d00af57178b392b68e690e
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-19 02:47:46 +0000

    Update styling for Others component

--- src/Components/Others.jsx
+++ src/Components/Others.jsx
@@ -16,9 +16,9 @@ function Others() {
                 return(
                     <div key={key} className='relative flex flex-col justify-center items-center'>
                         <img src={other.image} alt="image" className='w-[100%] h-[100%]'/>
-                        <div className={`absolute top-[22rem]`}>
-                        <h1 className='font-extrabold text-[2rem] mt-[3.5rem] mb-[1.5rem] font-FrauncesFont'>{other.topic}</h1>
-                        <p className='px-[1.3rem] text-[0.9rem] font-semibold font-BarlowFont'>{other.content}</p>
+                        <div className={`absolute top-[22rem] sm:top-[42rem]`}>
+                        <h1 className='font-extrabold text-[2rem] sm:text-[3.5rem] mt-[3.5rem] sm:mt-[4rem] mb-[1.5rem] sm:mb-[2rem] font-FrauncesFont'>{other.topic}</h1>
+                        <p className='px-[1.3rem] text-[0.9rem] sm:text-[1.4rem] font-semibold font-BarlowFont'>{other.content}</p>
                         </div>
                     </div>
                 )
