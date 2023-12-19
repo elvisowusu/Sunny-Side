133919af066799808406024c5ec964c5c8a7ed12
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-19 01:54:06 +0000

    Add custom colors and fonts to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,27 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "Softred": "hsl(7, 99%, 70%)",
+        "Yellow": "hsl(51, 100%, 49%)",
+        "Darkdesaturatedcyan(graphicdesigntext)": "hsl(167, 40%, 24%)",
+        "Darkblue(photographytext)": "hsl(198, 62%, 26%)",
+        "Darkmoderatecyan(footer)": "hsl(168, 34%, 41%)",
+        "Verydarkdesaturatedblue": "hsl(212, 27%, 19%)",
+        "Verydarkgrayishblue": "hsl(213, 9%, 39%)",
+        "Darkgrayishblue": "hsl(232, 10%, 55%)",
+        "Grayishblue": "hsl(210, 4%, 67%)",
+        "White": "hsl(0, 0%, 100%)",
+      },
+      fontFamily:{
+        "BarlowFont": "Barlow, sans-serif",
+        "FrauncesFont": "Fraunces, serif",
+      }
+    },
   },
   plugins: [],
 }
