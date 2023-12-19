6966759560b9c54df0619378037f43d76882d5d0
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-19 11:15:03 +0000

    Refactor NavItems and index.css

--- src/Components/NavItems.jsx
+++ src/Components/NavItems.jsx
@@ -6,7 +6,7 @@ export const NavItems = ()=>{
         <ul className="fixed NavItems ">
         <div className="fixed border-l-[2.5rem] border-l-transparent border-b-White border-b-[3rem] right-[1.37rem] top-[4.5rem] sm:top-[5.5rem] md:hidden"></div>
             {items.map((item,index)=>{
-                return <li className={`font-FrauncesFont ${index!=3?'OtherList':'contact'}`} key={index}>{item}</li>
+                return <li className={`${index!=3?'OtherList':'contact'}`} key={index}>{item}</li>
             })}
         </ul>
     )
