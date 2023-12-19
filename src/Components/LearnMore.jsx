213df95c8a4dee93ffa78d0472a4aa094a478cf7
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-19 12:19:45 +0000

    Refactor LearnMore component styling

--- src/Components/LearnMore.jsx
+++ src/Components/LearnMore.jsx
@@ -14,14 +14,14 @@ function LearnMore() {
         <div>{
                 info.map((info,key)=>{
                     return(
-                        <div key={key}>
+                        <div className='md:grid md:grid-cols-2 ' key={key}>
                             <img src={screenWidth < 768 ? info.image:info.image2 } alt="" />
-                            <div className='py-[4rem] sm:py-[6rem] flex flex-col items-center'>
-                                <h1 className='text-[2rem] sm:text-[2.5rem] px-[3rem] leading-[2.4rem] font-extrabold font-FrauncesFont mb-[1.6rem] sm:mb-[3rem] text-Verydarkdesaturatedblue'>{info.topic}</h1>
-                                <p className='px-[1.3rem] sm:text-[1.5rem] font-semibold mb-[2rem] sm:mb-[3.5rem] text-Darkgrayishblue'>{info.content}</p>
-                                <div className='leading-[0.2rem] sm:leading-[0rem]'>
-                                    <span className='font-extrabold tracking-wide font-FrauncesFont sm:text-[1.3rem] text-Verydarkdesaturatedblue'>LEARN MORE</span>
-                                    <hr className={`opacity-30 rounded border-none h-[0.6rem] sm:h-[0.8rem] w-[8rem] sm:w-[10.5rem] ${info.image ===mobileStandOut?'bg-Softred':'bg-Yellow'}`}/>
+                            <div className='py-[4rem] sm:py-[6rem] md:py-[6rem] flex flex-col items-center md:items-start md:text-left md:px-[4rem]'>
+                                <h1 className='text-[2rem] sm:text-[2.5rem] md:text-[2rem] px-[3rem] md:px-0 leading-[2.4rem] font-extrabold font-FrauncesFont mb-[1.6rem] sm:mb-[3rem] md:mb-[2rem] text-Verydarkdesaturatedblue'>{info.topic}</h1>
+                                <p className='px-[1.3rem] md:px-0 sm:text-[1.5rem] md:text-[1.1rem] font-semibold mb-[2rem] sm:mb-[3.5rem] md:mb-[2rem] text-Darkgrayishblue'>{info.content}</p>
+                                <div className='group/main leading-[0.2rem] sm:leading-[0rem] md:leading-[0.4rem] cursor-pointer'>
+                                    <span className='font-extrabold tracking-wide font-FrauncesFont sm:text-[1.3rem] md:text-[1.1rem] text-Verydarkdesaturatedblue'>LEARN MORE</span>
+                                    <hr className={`opacity-30 group-hover/main:opacity-50 rounded border-none h-[0.6rem] w-[8rem] sm:w-[10.5rem] md:w-[8.5rem] ${info.image ===mobileStandOut?'bg-Softred':'bg-Yellow'}`}/>
                                 </div>
                             </div>
                         </div>
