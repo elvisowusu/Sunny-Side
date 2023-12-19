bac75fb2edbab8f9a7e3fc17fb3fcf17896b8f8f
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-19 02:43:24 +0000

    Update NavItems styling for responsiveness

--- src/Components/NavItems.jsx
+++ src/Components/NavItems.jsx
@@ -4,7 +4,7 @@ export const NavItems = ()=>{
 
     return(
         <ul className="fixed NavItems ">
-        <div className="fixed border-l-[2.5rem] border-l-transparent border-b-White border-b-[3rem] right-[1.37rem] top-[4.5rem]"></div>
+        <div className="fixed border-l-[2.5rem] border-l-transparent border-b-White border-b-[3rem] right-[1.37rem] top-[4.5rem] sm:top-[5.5rem]"></div>
             {items.map((item,index)=>{
                 return <li className={`${index!=3?'OtherList':'contact'}`} key={index}>{item}</li>
             })}
